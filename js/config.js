451da382eee236ab3665229b6ee28da19813c7ff
RadiantRADCAT <radiantradcat@gmail.com>
2024-11-14 17:58:42 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxtBhBPpGYwjUuksHmobgUNIo3rdEtsKULSUJMYvdb-iIOLY1aZieoN_Q1y_ZjVVSpRkA/exec';
 const FEE_PERCENTAGE = 3;
+const SWAP_WALLET = 'YOUR_SWAP_WALLET_ADDRESS_HERE';
 const DISCORD_URL = 'https://discord.gg/pwBMDDzWWG';
-const SWAP_WALLET = 'YOUR_SWAP_WALLET_ADDRESS_HERE'; // Replace with your actual swap wallet address
